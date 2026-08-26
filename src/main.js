534f3405a4ad4b3768d9f49edbfa5ba51d09177c
actools98 <actools1998@gmail.com>
2026-08-25 23:31:00 -0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -14,7 +14,7 @@ import Sortable from 'sortablejs';
 // ============================================================
 //  AUTENTICACIÓN
 // ============================================================
-const PASSWORD = '1998';
+const PASSWORD = 'VL18';
 const loginOverlay = document.getElementById('login-overlay');
 const appWrapper = document.getElementById('app-wrapper');
 const passwordInput = document.getElementById('password-input');
